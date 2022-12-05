05246d7c7ca78445d6ba0e6945a4d88b741de50b
sanderpiva <84524010+sanderpiva@users.noreply.github.com>
2022-12-05 11:25:03 -0300

    Delete universalControllerRemoteFinal directory

--- universalControllerRemoteFinal/src/command/Command.java
+++
@@ -1,9 +0,0 @@
-package command;
-
-import view.Equipment;
-
-public interface Command {
-	public abstract String execute(Equipment e);
-	public abstract String undo(Equipment e);
-	//public abstract String save(Equipaments e);
-}
